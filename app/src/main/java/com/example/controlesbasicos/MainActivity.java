a83f42744411d05a24abad958354dc0d6e9bdd8e
carlos <saymonlenny@gmail.com>
2020-08-27 01:20:31 -0600

    Sensor de proximidad

--- app/src/main/java/com/example/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/example/controlesbasicos/MainActivity.java
@@ -1,37 +1,68 @@
 package com.example.controlesbasicos;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+    @Override
+    protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnSiguiente = (Button)findViewById(R.id.btnSiguiente);
-        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(sensor==null){
+            finish();
+        }
+        final TextView lblSensorProximidad = (TextView)findViewById(R.id.lblSensorProxim);
+        sensorEventListener = new SensorEventListener() {
             @Override
-            public void onClick(View view) {
-
-                Intent siguiente = new Intent(MainActivity.this,Inicio.class);
-                startActivity(siguiente);
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if( sensorEvent.values[0]>=0 && sensorEvent.values[0]<=4 ){
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                    lblSensorProximidad.setText("LEJOS: "+ sensorEvent.values[0]);
+                } else if(sensorEvent.values[0]>4 && sensorEvent.values[0]<=8 ){
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                    lblSensorProximidad.setText("INTERMEDIO: "+ sensorEvent.values[0]);
+                } else{
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                    lblSensorProximidad.setText("CERCA: "+ sensorEvent.values[0]);
+                }
             }
-        });
-    }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
 
+            }
+        };
+        iniciar();
+    }
+    void iniciar(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+    void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
 }
