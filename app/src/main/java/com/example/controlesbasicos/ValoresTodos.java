6150509fa98751cb541d8f346db4ef060dd3b303
Julio Rodriguez <jarp69721@gmail.com>
2020-09-04 18:37:08 -0600

    Conversor de area segunda parte

--- app/src/main/java/com/example/controlesbasicos/ValoresTodos.java
+++ app/src/main/java/com/example/controlesbasicos/ValoresTodos.java
@@ -7,6 +7,6 @@ public class ValoresTodos {
     Spinner val;
     double datos[][] = {
             new double[]{ }, //Valores de CONVERSOR UNIVERSAL
-            new double[] { }  //Valores de CONVERSOR AREA
+            new double[] {1, 0.1329421, 0.111111, 0.092903, 628.86, 0.0000132, 9.2903e-6}  //Valores de CONVERSOR AREA
     };
 }
