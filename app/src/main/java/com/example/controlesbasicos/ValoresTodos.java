f26ee89a6e2bede7b43edcf96a90c51fb9f95dc1
mator <kji>
2020-09-04 22:50:52 -0600

    modificacion

--- app/src/main/java/com/example/controlesbasicos/ValoresTodos.java
+++ app/src/main/java/com/example/controlesbasicos/ValoresTodos.java
@@ -7,6 +7,6 @@ public class ValoresTodos {
     Spinner val;
     double datos[][] = {
             new double[]{ }, //Valores de CONVERSOR UNIVERSAL
-            new double[] {1, 0.1329421, 0.111111, 0.092903, 628.86, 0.0000132, 9.2903e-6}  //Valores de CONVERSOR AREA
+            new double[] {1, 0.1111, 0.111111, 0.092903, 0.000147, 0.000013, 9.2903e-6}  //Valores de CONVERSOR AREA
     };
 }
