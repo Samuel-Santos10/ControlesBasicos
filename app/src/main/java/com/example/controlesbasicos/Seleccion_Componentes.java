0dc37c15738fca9ea6a439259f4d77aec15c09db
Carlos <saymonlenny@gmail.com>
2020-11-15 02:49:37 -0600

    nuevos cambios

--- app/src/main/java/com/example/controlesbasicos/Seleccion_Componentes.java
+++ app/src/main/java/com/example/controlesbasicos/Seleccion_Componentes.java
@@ -2,7 +2,11 @@ package com.example.controlesbasicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Seleccion_Componentes extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class Seleccion_Componentes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion__componentes);
+
+        try {
+            Button btnSelecCPU = (Button) findViewById(R.id.btnSelecCPU);
+            btnSelecCPU.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    informacionBasica();
+                }
+            });
+
+        }catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Error al ingresar en la opcion.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    void informacionBasica(){
+        Intent admiCpu = new Intent(Seleccion_Componentes.this, AdmiCPU.class);
+        startActivity(admiCpu);
     }
 }
---
+++ app/src/main/java/com/example/controlesbasicos/adaptadorImagenes.java
@@ -0,0 +1,65 @@
+package com.example.controlesbasicos;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class adaptadorImagenes extends BaseAdapter {
+
+    Context context;
+    ArrayList<productos> datos;
+    LayoutInflater layoutInflater;
+    productos producto;
+
+    public adaptadorImagenes(Context context, ArrayList<productos> datos){
+        this.context = context;
+        try {
+            this.datos = datos;
+        }catch (Exception ex){}
+    }
+    @Override
+    public int getCount() {
+        try {
+            return datos.size();
+        }catch (Exception ex) {
+            return 0;
+        }
+    }
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        layoutInflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View itemView = layoutInflater.inflate(R.layout.item_mostrar, viewGroup, false);
+        TextView tvTituMarca = (TextView)itemView.findViewById(R.id.TVlmarca);
+        TextView tvTituDescripcion = (TextView)itemView.findViewById(R.id.TVldescripcion);
+        TextView tvTituPrecio = (TextView)itemView.findViewById(R.id.TVlprecio);
+
+        ImageView imageView = (ImageView)itemView.findViewById(R.id.img);
+        try {
+            producto = datos.get(i);
+            tvTituMarca.setText(producto.getNombre());
+            tvTituDescripcion.setText(producto.getMarca());
+            tvTituPrecio.setText(producto.getPrecio());
+
+            Bitmap imageBitmap = BitmapFactory.decodeFile(producto.getUrlImg());
+
+            imageView.setImageBitmap(imageBitmap);
+        }catch (Exception ex){ }
+        return itemView;
+    }
+}
