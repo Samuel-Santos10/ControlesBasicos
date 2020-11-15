62daec420ab815d21eb96da354aefcb13a09b0a8
Julio Rodriguez <jarp69721@gmail.com>
2020-11-15 04:14:14 -0600

    botones de navegacion finish

--- app/src/main/java/com/example/controlesbasicos/Seleccion_Componentes.java
+++ app/src/main/java/com/example/controlesbasicos/Seleccion_Componentes.java
@@ -23,7 +23,48 @@ public class Seleccion_Componentes extends AppCompatActivity {
                     informacionBasica();
                 }
             });
+            Button btnSelecMotherboard = (Button) findViewById(R.id.btnSelecMother);
+            btnSelecMotherboard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    informacionMother();
 
+
+                }
+            });
+
+            Button btnSelecGPU = (Button) findViewById(R.id.btnSelecGPU);
+            btnSelecGPU.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    informacionGpu();
+                }
+            });
+
+            Button btnSelecRAM = (Button) findViewById(R.id.btnSelecRAM);
+            btnSelecRAM.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    informacionRam();
+                }
+            });
+
+            Button btnSelecDISCO = (Button) findViewById(R.id.btnSelecDisco);
+            btnSelecDISCO.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    informacionDisco();
+                }
+            });
+
+            Button btnSelecFuente = (Button) findViewById(R.id.btnSelecFuente);
+            btnSelecFuente.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    informacionFuente();
+                }
+            });
+            //aqui ño
         }catch (Exception ex){
             Toast.makeText(getApplicationContext(), "Error al ingresar en la opcion.", Toast.LENGTH_SHORT).show();
         }
@@ -32,5 +73,29 @@ public class Seleccion_Componentes extends AppCompatActivity {
     void informacionBasica(){
         Intent admiCpu = new Intent(Seleccion_Componentes.this, AdmiCPU.class);
         startActivity(admiCpu);
+    }
+
+    void informacionMother(){
+        Intent admiMotherboard = new Intent(Seleccion_Componentes.this, AdmiMotherboard.class);
+        startActivity(admiMotherboard);
+    }
+    void informacionGpu(){
+        Intent admiGpu = new Intent(Seleccion_Componentes.this, AdmiGPU.class);
+        startActivity(admiGpu);
+    }
+
+    void informacionRam(){
+        Intent admiRam = new Intent(Seleccion_Componentes.this, AdmiRAM.class);
+        startActivity(admiRam);
+    }
+
+    void informacionDisco(){
+        Intent admiDisco = new Intent(Seleccion_Componentes.this, AdmiDisco.class);
+        startActivity(admiDisco);
+    }
+
+    void informacionFuente(){
+        Intent admiFuente = new Intent(Seleccion_Componentes.this, AdmiFuente.class);
+        startActivity(admiFuente);
     }
 }
