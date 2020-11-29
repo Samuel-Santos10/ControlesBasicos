d76a5d82c09cc344237d32a7864cd68f58d16cb3
Julio Rodriguez <jarp69721@gmail.com>
2020-11-29 11:08:49 -0600

    finish button

--- app/src/main/java/com/example/controlesbasicos/DB_PC.java
+++ app/src/main/java/com/example/controlesbasicos/DB_PC.java
@@ -15,6 +15,10 @@ public class DB_PC extends SQLiteOpenHelper {
     static String tblProductos = "CREATE TABLE productos(idproducto integer primary key autoincrement, nombre text, marca text, stock int, precio real, url text)"; // este es de CPU
     static   String tblMotherboard = "CREATE TABLE motherboard(idplaca integer primary key autoincrement, nombre text, marca text, stock int, precio real, url text)";
     static String tblDisco = "CREATE TABLE Disco(iddisco integer primary key autoincrement, nombre text, marca text, stock int, precio real, url text)";
+    static String tblRam = "CREATE TABLE Ram(idRam integer primary key autoincrement, nombre text, marca text, stock int, precio real, url text)";
+    static String tblFuente = "CREATE TABLE Fuente(idFuente integer primary key autoincrement, nombre text, marca text, stock int, precio real, url text)";
+    static String tblGpu = "CREATE TABLE Gpu(idGpu integer primary key autoincrement, nombre text, marca text, stock int, precio real, url text)";
+
 
     public DB_PC(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, nameDB, factory, version); // nameDB = La creacion de la base de datos en SQLite
@@ -25,8 +29,9 @@ public class DB_PC extends SQLiteOpenHelper {
         db.execSQL(tblProductos);
         db.execSQL(tblMotherboard);
         db.execSQL(tblDisco);
-
-
+        db.execSQL(tblRam);
+        db.execSQL(tblFuente);
+        db.execSQL(tblGpu);
     }
 
 
@@ -107,6 +112,82 @@ public class DB_PC extends SQLiteOpenHelper {
                 break;
             case "Eliminar":
                 sqLiteDatabasewritable.execSQL("DELETE FROM Disco WHERE iddisco='"+ data[0] +"'");
+                break;
+            default:
+                break;
+        }
+        return cursor;
+    }
+
+    //RAM
+
+    public Cursor mantenimientoRam(String accion, String[] data){
+        SQLiteDatabase sqLiteDatabaseReadable = getReadableDatabase();
+        SQLiteDatabase sqLiteDatabasewritable = getWritableDatabase();
+        Cursor cursor = null;
+        switch (accion){
+            case "Consultar":
+                cursor = sqLiteDatabaseReadable.rawQuery("SELECT * FROM Ram ORDER BY nombre ASC", null);
+                break;
+            case "Nuevo":
+                sqLiteDatabasewritable.execSQL("INSERT INTO Ram(nombre,marca,stock,precio,url) VALUES('"+ data[1] +"','"+data[2]+"','"+data[3]+"','"+data[4]+"','"+data[5]+"')");
+                break;
+            case "Modificar":
+                sqLiteDatabasewritable.execSQL("UPDATE Ram SET nombre='"+ data[1] +"',marca='"+data[2]+"',stock='"+data[3]+"',precio='"+data[4]+"', url='"+data[5]+"' WHERE idRam='"+data[0]+"'");
+                break;
+            case "Eliminar":
+                sqLiteDatabasewritable.execSQL("DELETE FROM Ram WHERE idRam='"+ data[0] +"'");
+                break;
+            default:
+                break;
+        }
+        return cursor;
+    }
+
+
+    //FUENTE
+
+    public Cursor mantenimientoFuente(String accion, String[] data){
+        SQLiteDatabase sqLiteDatabaseReadable = getReadableDatabase();
+        SQLiteDatabase sqLiteDatabasewritable = getWritableDatabase();
+        Cursor cursor = null;
+        switch (accion){
+            case "Consultar":
+                cursor = sqLiteDatabaseReadable.rawQuery("SELECT * FROM Fuente ORDER BY nombre ASC", null);
+                break;
+            case "Nuevo":
+                sqLiteDatabasewritable.execSQL("INSERT INTO Fuente(nombre,marca,stock,precio,url) VALUES('"+ data[1] +"','"+data[2]+"','"+data[3]+"','"+data[4]+"','"+data[5]+"')");
+                break;
+            case "Modificar":
+                sqLiteDatabasewritable.execSQL("UPDATE Fuente SET nombre='"+ data[1] +"',marca='"+data[2]+"',stock='"+data[3]+"',precio='"+data[4]+"', url='"+data[5]+"' WHERE idFuente='"+data[0]+"'");
+                break;
+            case "Eliminar":
+                sqLiteDatabasewritable.execSQL("DELETE FROM Fuente WHERE idFuente='"+ data[0] +"'");
+                break;
+            default:
+                break;
+        }
+        return cursor;
+    }
+
+    //GPU
+
+    public Cursor mantenimientoGpu(String accion, String[] data){
+        SQLiteDatabase sqLiteDatabaseReadable = getReadableDatabase();
+        SQLiteDatabase sqLiteDatabasewritable = getWritableDatabase();
+        Cursor cursor = null;
+        switch (accion){
+            case "Consultar":
+                cursor = sqLiteDatabaseReadable.rawQuery("SELECT * FROM Gpu ORDER BY nombre ASC", null);
+                break;
+            case "Nuevo":
+                sqLiteDatabasewritable.execSQL("INSERT INTO Gpu(nombre,marca,stock,precio,url) VALUES('"+ data[1] +"','"+data[2]+"','"+data[3]+"','"+data[4]+"','"+data[5]+"')");
+                break;
+            case "Modificar":
+                sqLiteDatabasewritable.execSQL("UPDATE Gpu SET nombre='"+ data[1] +"',marca='"+data[2]+"',stock='"+data[3]+"',precio='"+data[4]+"', url='"+data[5]+"' WHERE idGpu='"+data[0]+"'");
+                break;
+            case "Eliminar":
+                sqLiteDatabasewritable.execSQL("DELETE FROM Gpu WHERE idGpu='"+ data[0] +"'");
                 break;
             default:
                 break;
