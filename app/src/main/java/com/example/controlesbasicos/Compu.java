9ec07aaf425e3ef437f95f08d5aeeb0c9f3b91bb
KevinRivera717 <k.r.rivera99@gmail.com>
2020-12-13 18:48:20 -0600

    diseño_final

--- app/src/main/java/com/example/controlesbasicos/Compu.java
+++ app/src/main/java/com/example/controlesbasicos/Compu.java
@@ -3,11 +3,19 @@ package com.example.controlesbasicos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Compu extends AppCompatActivity {
+
+    //Iniciamos variable
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,41 @@ public class Compu extends AppCompatActivity {
                 atras();
             }
         });
+        //asignamos la varible
+        textView = findViewById(R.id.etmConcepto5);
+
+        //Iciamos el color de los dravable
+
+        ColorDrawable leftBorder = new ColorDrawable(Color.RED);
+        ColorDrawable topBorder = new ColorDrawable(Color.GREEN);
+        ColorDrawable righBorder = new ColorDrawable(Color.BLUE);
+        ColorDrawable bottonBoder = new ColorDrawable(Color.YELLOW);
+        ColorDrawable background = new ColorDrawable(Color.WHITE);
+
+        //Icializamos Dravables Array
+
+        Drawable[] layers = new Drawable[] {
+                leftBorder, //Red color
+                topBorder, // Green Color
+                righBorder, //Blue Color
+                bottonBoder, //Yellow Color
+                background, // White Color
+        };
+        //inicializamos LayerDravable
+        LayerDrawable layerDrawable = new LayerDrawable(layers);
+        //Drav left Border
+        layerDrawable.setLayerInset(0,0,0,15,0);
+
+        layerDrawable.setLayerInset(1,15,0,0,15);
+
+        layerDrawable.setLayerInset(2,15,15,0,0);
+
+        layerDrawable.setLayerInset(3,15,15,15,0);
+
+        layerDrawable.setLayerInset(4,15,15,15,15);
+
+        //set Background
+        textView.setBackground(layerDrawable);
     }
     void atras(){
         Intent volver = new Intent(Compu.this, InformacionBasica.class);
--- app/src/main/java/com/example/controlesbasicos/Ram.java
+++ app/src/main/java/com/example/controlesbasicos/Ram.java
@@ -3,11 +3,18 @@ package com.example.controlesbasicos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Ram extends AppCompatActivity {
+    //Iniciamos variable
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,43 @@ public class Ram extends AppCompatActivity {
                 VolverInfoBa();
             }
         });
+
+        //asignamos la varible
+        textView = findViewById(R.id.etmConcepto);
+
+        //Iciamos el color de los dravable
+
+        ColorDrawable leftBorder = new ColorDrawable(Color.RED);
+        ColorDrawable topBorder = new ColorDrawable(Color.GREEN);
+        ColorDrawable righBorder = new ColorDrawable(Color.BLUE);
+        ColorDrawable bottonBoder = new ColorDrawable(Color.YELLOW);
+        ColorDrawable background = new ColorDrawable(Color.WHITE);
+
+        //Icializamos Dravables Array
+
+        Drawable [] layers = new Drawable[] {
+                leftBorder, //Red color
+                topBorder, // Green Color
+                righBorder, //Blue Color
+                bottonBoder, //Yellow Color
+                background, // White Color
+        };
+        //inicializamos LayerDravable
+        LayerDrawable layerDrawable = new LayerDrawable(layers);
+        //Drav left Border
+        layerDrawable.setLayerInset(0,0,0,15,0);
+
+        layerDrawable.setLayerInset(1,15,0,0,15);
+
+        layerDrawable.setLayerInset(2,15,15,0,0);
+
+        layerDrawable.setLayerInset(3,15,15,15,0);
+
+        layerDrawable.setLayerInset(4,15,15,15,15);
+
+        //set Background
+        textView.setBackground(layerDrawable);
+
     }
     void VolverInfoBa(){
         Intent Retroceder = new Intent(Ram.this, InformacionBasica.class);
